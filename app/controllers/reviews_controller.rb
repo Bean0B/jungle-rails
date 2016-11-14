06035a5e5802241c37466fec1eae2f5b77b7656e
vagrant <aileentobrien@gmail.com>
2016-11-13 21:14:59 -0800

    Users can now delete their own reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,14 +1,38 @@
 class ReviewsController < ApplicationController
+  before_action :authenticate
+
 
   def create
     @product = Product.find(params[:product_id])
-    @review = @product.reviews.create(review_params)
+    @review = @product.reviews.new(review_params)
+    @review.user_id = current_user.id
+    if @review.save
+      redirect_to product_path(@product)
+    else
+      redirect_to product_path(@product)
+    end
+  end
+
+  def destroy
+    @product = Product.find(params[:product_id])
+    @review = @product.reviews.find(params[:id])
+    @review.destroy
     redirect_to product_path(@product)
   end
 
   private
+
+  def authenticate
+    if current_user.nil?
+      redirect_to login_path(@product)
+    end
+  end
+
     def review_params
-      params.require(:review).permit(:user, :rating, :description, :product)
+      params.require(:review).permit(
+        :rating,
+        :description,
+      )
     end
 end
 
